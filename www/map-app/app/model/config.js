bdec4cb66172b2dfb30a8adc4176c7afd58c84ed
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-03-17 15:55:36 +0000

    config.js - implement config setters
    This is so that we can update the defaults where appropriate.

--- www/map-app/app/model/config.js
+++ www/map-app/app/model/config.js
@@ -101,9 +101,10 @@ define([
 
   console.log(version_json);
   console.log(about_html);
-  
-  const methods = {};
-  const data = {};
+
+  const accessors = {}; // config setters and getters, indexed by id, then by 'get' or 'set'
+  const methods = {}; // Exported methods
+  const data = {}; // The config data, indexed by id
 
   // Dynamically construct the config object from the schema
   configSchema.forEach((def) => {
@@ -111,6 +112,9 @@ define([
     let getterName = def.id;
     let getter = def.getter;
 
+    accessors[def.id] = {};
+    
+    // Define getters by default.
     // Adjust the getter based on the config schema
     if (typeof(getter) === 'function') {
       // A function, bind it's `this` to data.
@@ -136,12 +140,66 @@ define([
     }
     
     methods[getterName] = getter;
+    accessors[def.id].get = getter;
+    
+    // Allow setters to be defined, but not by default
+    if (def.setter !== undefined) {
+      let setter = def.setter;
+      let setterName;
+      
+      // Adjust the setter based on the config schema
+      if (typeof(setter) === 'function') {
+        // A function, bind it's `this` to data.
+        setter = setter.bind(data); 
+        if (setter.name
+            && setter.name !== 'anonymous') {
+          // A named function, use its name.
+          setterName = setter.name;
+        }
+      }
+      else if (typeof(setter) === 'string') {
+        // A string, use it as the getter name and a default getter implementation.
+        setterName = setter;
+        setter = (val) => { data[def.id] = val };
+      }
+      else {
+        // Anything else defined...
+        throw new Error(`config schema for ${def.id}.setter must be a string or a function`);
+      }
+
+      methods[setterName] = setter;
+      accessors[def.id].set = setter;
+    }
 
     // Initialise the config value
     if (def.init) {
       data[def.id] = def.init();
     }
   });
+
+
+  // Define an 'add' method, which sets any settable items in the
+  // object passed to the init function
+  methods.add = (cfg) => {
+    if (typeof(cfg) !== 'object')
+      throw new Error(`argument to add must be an object`);
+    console.log("add", cfg);    
+    Object.keys(cfg).forEach(id => {
+      const def = configSchema.find((e) => id === e.id);
+      if (!def) {
+        console.log(`ignoring invalid config item ${id}`);
+        return;
+      }
+      
+      const setter = accessors[id].set;
+      if (!setter) {
+        console.log(`ignoring unsettable config item ${id}`);
+        return;
+      }
+      
+      setter.call(data, cfg[id]);
+    });
+  };
   
   return methods; // hides the data by closing over it
 });
