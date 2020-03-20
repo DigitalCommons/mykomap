c57c7839917d6c922b9dae741a0f01dcf80cdff1
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2020-03-20 20:02:55 +0000

    app/model/config_schema.js - expand definitions, include doc generating func
    Drop the config attribute `defaultNongeoLatLng`, nothing seems to use it.

--- www/map-app/app/model/config_schema.js
+++ www/map-app/app/model/config_schema.js
@@ -1,9 +1,17 @@
 /**
  *
- * The purpose of this module is to insulate the rest of the application from
- * changes made to the format of config_json, version_json, etc.
+ * The purpose of this module is to define the configuration parameters, in a self-documenting manner.
  *
- * It returns an object with accessor methods for obtaining configured data.
+ * It returns a function which accepts named values defining the
+ * defaults for certain values which are obtained
+ * programmatically in config.js.
+ *
+ * It is intended to be read by the script `generate-config-doc` to
+ * generate a documentation file to commit as `CONFIG.md`. This is a
+ * way of avoiding needing to duplicate the documentation manually in
+ * more than one place, and possibly having them become inconsistent.
+ *
+ * REMEMBER TO REGENERATE CONFIG.md IF YOU ALTER THIS SCHEMA.
  */
 
 
@@ -12,12 +20,20 @@ define([], function() {
 
   /** Define config value types, and certain helper functions.
    *
+   * Type 'name' should be a JSDoc description:
+   * https://jsdoc.app/tags-type.html
+   *
+   * Additional information in 'descr', or 'stringDescr' for how
+   * strings get parsed. These are optional.
+   *
    * parseString should accept a string and return a parsed value
    * suitable for the config item of associated type.
    */
   const types = {
     boolean: {
-      // Parse '', 'false', or 'no' as false, everything else as true.
+      name: '{boolean}',
+      stringDesc: "The empty string, 'false', or 'no' parse as `false`, "+
+        "everything else as `true`.",
       parseString: (val) => {
         switch(val.toLowerCase()) {
         case '':
@@ -30,29 +46,35 @@ define([], function() {
       },
     },
     string: {
+      name: '{string}',
       // No-op
       parseString: (val) => val,
     },
-    latLng:  {
-      // Parse as a list of 2 comma-delimited numbers.
-      // Like: "<latitude>,<longitude>"
-      // Drop trailing numbers
+    latLng: {
+      name: '{number[]}',
+      descr: 'A two-element array defining latitude and longitude in degrees.',
+      stringDescr: 'A comma-delimited list of two numbers defining latitude '+
+        'and longitude in degrees.',
       parseString: (val) =>
         val.split(',', 2).map(s => isNaN(s)? 0 : Number(s)),
     },
-    latLng2:  {
-      // Parse as a list of 4 comma-delimited numbers.
-      // Like: "<latitude1>,<longitude1>,<latitude2>,<longitude2>"
-      // Drop trailing numbers
+    latLng2: {
+      name: '{number[][]}',
+      descr: '[[latitude, longitude],[latitude, longitude]] - '+
+        'A two-element array of two-element arrays of numbers, '+
+        'defining two pairs of latitude and longitudes in degrees',
+      stringDescr: 'A comma-delimited list of four numbers defining two latitude '+
+        'and longitude pairs, in degrees.',
       parseString: (val) => {
         const e = val.split(',', 4).map(s => isNaN(s)? 0 : Number(s));
         return [[e[0],e[1]],[e[2],e[3]]];
       },
     },
     arrayOfString:  {
-      // Parse as comma-delimited values.
-      // NB doesn't handle escaping, so no ',' possible in strings
-      // Spaces are considered significant - they are not trimmed.
+      name: '{string[]}',
+      descr: 'An array of strings.',
+      stringDescr: 'A comma-delimited list of strings. No escaping is used, '+
+        "so no commas can exist in the strings. Spaces are not trimmed.",
       parseString: (val) => val.split(/,/),
     },
   };
@@ -76,58 +98,72 @@ define([], function() {
    * object). If one is defined, the name of the function will be used
    * as the name of the getter. If this value is unset, the getter
    * method has the same name as the id.
+   *
+   * Note, the schema is defined as a function accepting named values
+   * to initialise certain of the attributes if they are not
+   * present. These should all be supplied by config.js. If defaultDescr
+   * is defined, this is used as a documentation of the default value,
+   * else the value itself is quoted.
    */
   const configSchema = ({
-    aboutHtml, variant, timestamp, gitcommit, namedDatasets,
-    htmlTitle, defaultNongeoLatLng, initialBounds, filterableFields,
-    doesDirectoryHaveColours, disableClusteringAtZoom
+    aboutHtml,
+    variant,
+    timestamp,
+    gitcommit,
+    namedDatasets,
+    htmlTitle,
+    initialBounds,
+    filterableFields,
+    doesDirectoryHaveColours,
+    disableClusteringAtZoom,
   } = {}) => [
     { id: 'aboutHtml',
-      descr: `Raw HTML definition of the map's "about" text`,
+      descr: `Raw HTML definition of the map's "about" text.`,
+      defaultDescr: "The contents of the consuming project's file `config/about.html`",
       init: () => aboutHtml,
+      type: types.string,
     },
     { id: 'servicesPath',
-      descr: 'Preset location of the data source script(s)',
+      descr: 'Preset location of the data source script(s).',
       init: () => 'services/',
       getter: 'getServicesPath',
+      type: types.string,
     },
     { id: 'variant',
-      descr: 'The name of the variant used to generate this map application',
+      descr: 'The name of the variant used to generate this map application.',
+      defaultDescr: "Defined by `variant` attribute of the consuming project's "+
+      "file `config/version.json`",
       init: () => variant,
       getter: 'getSoftwareVariant',
+      type: types.string,
     },
     { id: 'timestamp',
-      descr: 'A timestamp indicating when this application was deployed',
+      descr: 'A timestamp string indicating when this application was deployed.',
+      defaultDescr: "Defined by `variant` attribute of the consuming project's "+
+      "file `config/version.json`",
       init: () => timestamp,
       getter: 'getSoftwareTimestamp',
+      type: types.string,
     },
     { id: 'gitcommit',
-      descr: 'The git commit-ID of the sea-map source code deployed',
+      descr: 'The git commit-ID of the sea-map source code deployed.',
+      defaultDescr: "Defined by `variant` attribute of the consuming project's "+
+      "file `config/version.json`",
       init: () => gitcommit,
       getter: 'getSoftwareGitCommit',
+      type: types.string,
     },
     { id: 'namedDatasets',
-      descr: 'These names correspond to directories in www/services which contain '+
-      'default-graph-uri.txt, endpoint.txt, query.rq',
+      descr: 'A list of names that correspond to directories in www/services, which must contain '+
+      'default-graph-uri.txt, endpoint.txt, query.rq.',
       init: () => namedDatasets,
+      type: types.arrayOfString,
     },
     { id: 'htmlTitle',
-      descr: `If set, this will override the default value for the map's HTML <title> tag`,
+      descr: `If set, this will override the default value for the map's HTML <title> tag.`,
       init: () => htmlTitle,
       setter: 'setHtmlTitle',
       type: types.string,
-    },
-    { id: 'defaultNongeoLatLng',
-      descr: 'The default latitude and longitude values (as a '+
-      '`{lat: <string>, lng: <string>}` object) for initiatives with no address; '+
-      'defaults to `{lat: undefined, lng: undefined}`',
-      init: () => defaultNongeoLatLng,
-      getter: function getNongeoLatLng() {
-        return this.defaultNongeoLatLng?
-          this.defaultNongeoLatLng  : { lat: undefined, lng: undefined };
-      },
-      setter: 'setDefaultNongeoLatLng',
-      type: types.latLng,
     },
     { id: 'initialBounds',
       descr: 'The initial bounds of the map as an array: [[n1,e1],[n2,e2]]; '+
@@ -158,6 +194,51 @@ define([], function() {
       type: types.boolean,
     },
   ];
+
+  // This generates the documentation for this schema, in Markdown
+  configSchema.doc = () => [`
+# \`config.json\`
+
+This describes the schema of the \`config.json\` file consuming projects should supply,
+and valid attributes thereof. It should be an object definition. Valid attributes are
+described here. Valid attributes which are not defined in \`config.json\` will take a
+default value. Those which are not valid are ignored.
+
+Here is an example of what you might put in this file:
+
+\`\`\`
+ {
+  "namedDatasets": ["oxford"],
+  "htmlTitle": "Solidarity Oxford",
+  "filterableFields": [{ "field": "primaryActivity", "label": "Activities" }],
+  "doesDirectoryHaveColours": true,
+  "disableClusteringAtZoom": false
+}
+\`\`\`
+
+These values are defaults, however, which can be overridden in various ways,
+as described in [README.md](README.md)
+
+## Attributes
+
+The following attributes can be defined.
+
+`]
+  // This maps each definition into s documentation section
+    .concat(configSchema().map((def) => `
+### \`${def.id}\`
+
+- *type:* \`${def.type.name}\` ${def.type.desc || ''}
+- *in string context:* ${def.type.stringDescr || 'parsed as-is'}
+- *default:* ${def.defaultDescr || '`'+def.init()+'`'}
+- *settable?:* ${def.setter? 'yes' : 'no'}
+
+${def.descr}
+
+${def.details || ''}
+
+`))
+    .join('');
 
   return configSchema;
 });
