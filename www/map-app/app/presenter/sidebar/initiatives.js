1ef7c1678f543739cbe0d0f5c15e6c0d5edaeab0
Dean <dean.samarjiev@gmail.com>
2020-08-10 13:46:19 +0100

    fixed searching for hidden initiatives

--- www/map-app/app/presenter/sidebar/initiatives.js
+++ www/map-app/app/presenter/sidebar/initiatives.js
@@ -4,41 +4,41 @@ define([
   "model/sse_initiative",
   "presenter/sidebar/base",
   "presenter/map"
-], function(eventbus, config, sseInitiative, sidebarPresenter,map) {
+], function (eventbus, config, sseInitiative, sidebarPresenter, map) {
   "use strict";
 
   function StackItem(initiatives) {
     this.initiatives = initiatives;
   }
-  StackItem.prototype.isSearchResults = function() {
+  StackItem.prototype.isSearchResults = function () {
     // TODO - surely there's a more direct way to decide if this is a SearchResults object?
     return this.hasOwnProperty("searchString");
   };
 
-  function SearchResults(initiatives, searchString,filters=[]) {
+  function SearchResults(initiatives, searchString, filters = []) {
     // isa StackItem
     StackItem.call(this, initiatives);
     this.searchedFor = searchString;
-    this.searchString = filters.length > 0? 
-    "\""+searchString + "\" in " + filters.join(", ")
-    : "\""+searchString + "\"";
+    this.searchString = filters.length > 0 ?
+      "\"" + searchString + "\" in " + filters.join(", ")
+      : "\"" + searchString + "\"";
   }
   SearchResults.prototype = Object.create(StackItem.prototype);
 
-  function Presenter() {}
+  function Presenter() { }
 
   var proto = Object.create(sidebarPresenter.base.prototype);
 
-  proto.currentItem = function() {
+  proto.currentItem = function () {
     return this.contentStack.current();
   };
 
-  proto.currentItemExists = function() {
+  proto.currentItemExists = function () {
     // returns true only if the contentStack is empty
     return typeof (this.contentStack.current() !== "undefined" || this.contentStack.current() != null);
   };
-  proto.notifyMarkersNeedToShowNewSelection = function(lastContent,newContent = null) {
-    if(!newContent){
+  proto.notifyMarkersNeedToShowNewSelection = function (lastContent, newContent = null) {
+    if (!newContent) {
       newContent = this.contentStack.current().initiatives
     }
     eventbus.publish({
@@ -50,12 +50,12 @@ define([
   };
   function arrayMax(array) {
     return array.reduce((a, b) => Math.max(a, b));
-    
+
   }
   function arrayMin(array) {
     return array.reduce((a, b) => Math.min(a, b));
   }
-  proto.notifyMapNeedsToNeedsToBeZoomedAndPanned = function(sidebarWidth) {
+  proto.notifyMapNeedsToNeedsToBeZoomedAndPanned = function (sidebarWidth) {
     const initiatives = this.contentStack.current().initiatives;
     sidebarWidth = sidebarWidth || 0;
     const lats = initiatives.map(x => x.lat);
@@ -79,7 +79,7 @@ define([
     }
   };
 
-  proto.notifyMapNeedsToNeedsToBeZoomedAndPannedOneInitiative = function(initiative,sidebarWidth) {
+  proto.notifyMapNeedsToNeedsToBeZoomedAndPannedOneInitiative = function (initiative, sidebarWidth) {
     const initiatives = [initiative];
     const lats = initiatives.map(x => x.lat);
     const lngs = initiatives.map(x => x.lng);
@@ -105,74 +105,74 @@ define([
   };
 
 
-  proto.notifyShowInitiativeTooltip = function(initiative) {
+  proto.notifyShowInitiativeTooltip = function (initiative) {
     eventbus.publish({
       topic: "Map.needToShowInitiativeTooltip",
       data: initiative
     });
   };
-  proto.notifyHideInitiativeTooltip = function(initiative) {
+  proto.notifyHideInitiativeTooltip = function (initiative) {
     eventbus.publish({
       topic: "Map.needToHideInitiativeTooltip",
       data: initiative
     });
   };
-  proto.notifySidebarNeedsToShowInitiatives = function() {
+  proto.notifySidebarNeedsToShowInitiatives = function () {
     eventbus.publish({ topic: "Sidebar.showInitiatives" });
   };
-  proto.historyButtonsUsed = function(lastContent) {
+  proto.historyButtonsUsed = function (lastContent) {
     //console.log("sidebar/initiatives historyButtonsUsed");
     //console.log(lastContent);
     //this.notifyMarkersNeedToShowNewSelection(lastContent);
     this.view.refresh();
   };
 
-  proto.onInitiativeResults = function(data) {
+  proto.onInitiativeResults = function (data) {
     // TODO - handle better when data.results is empty
     //        Prob don't want to put them on the stack?
     //        But still need to show the fact that there are no results.
-
     //get the uniquids of the applied filters
     const filterKeys = Object.keys(map.getFilteredMap());
 
     //go in if there are any filters
-    if(filterKeys.length != 0){
+    if (filterKeys.length != 0) {
       //get the intersection of the filtered content and the search data
       //search results should be a subset of filtered
-      data.results = data.results.filter(i=> filterKeys.includes(i.uniqueId));
+      data.results = data.results.filter(i => filterKeys.includes(i.uniqueId));
     }
 
     //filter
-    this.contentStack.append(new SearchResults(data.results, data.text,map.getFiltersVerbose()));
+    this.contentStack.append(new SearchResults(data.results, data.text, map.getFiltersVerbose()));
 
-    if(data.results.length == 1){
+    //highlight markers on search results 
+    //reveal all potentially hidden markers before zooming in on them 
+    eventbus.publish({
+      topic: "Map.addSearchFilter",
+      data: { initiatives: data.results }
+    });
+
+    if (data.results.length == 1) {
       this.notifyMapNeedsToNeedsToBeZoomedAndPannedOneInitiative(data.results[0]);
     }
-    else if (data.results.length == 0){
+    else if (data.results.length == 0) {
       //do nothing on failed search
       console.log("no results");
     }
-    else{
+    else {
       //this.notifyMarkersNeedToShowNewSelection(lastContent);
       //deselect all
       this.notifyMapNeedsToNeedsToBeZoomedAndPanned(); //does not do anything?
     }
 
-    //highlight markers on search results 
-    eventbus.publish({
-      topic: "Map.addSearchFilter",
-      data: {initiatives: data.results}
-    });
-
     this.notifySidebarNeedsToShowInitiatives();
     this.view.refresh();
   };
 
-  proto.getFilterNames = function(){
+  proto.getFilterNames = function () {
     return map.getFiltersVerbose();
-   };
+  };
 
-  proto.initClicked= function(initiative){
+  proto.initClicked = function (initiative) {
     eventbus.publish({
       topic: "Directory.InitiativeClicked",
       data: initiative
@@ -180,32 +180,32 @@ define([
     if (window.outerWidth <= 800) {
       eventbus.publish({
         topic: "Directory.InitiativeClickedSidebar.hideSidebar",
-        data: {initiative: initiative}
+        data: { initiative: initiative }
       });
     }
   };
 
-  proto.onInitiativeClickedInSidebar = function(data) {
+  proto.onInitiativeClickedInSidebar = function (data) {
     console.log(data)
 
     const initiative = data.initiative;
     //this.contentStack.append(new StackItem([initiative]));
     //console.log(this.contentStack.current());
-    
+
     this.notifyMapNeedsToNeedsToBeZoomedAndPannedOneInitiative(initiative);
     this.view.refresh();
     eventbus.publish({
       topic: "Initiatives.searchedInitiativeClicked",
-      data: {initiative: data.initiative}
+      data: { initiative: data.initiative }
     });
   };
-  proto.onInitiativeMouseoverInSidebar = function(initiative) {
+  proto.onInitiativeMouseoverInSidebar = function (initiative) {
     this.notifyShowInitiativeTooltip(initiative);
   };
-  proto.onInitiativeMouseoutInSidebar = function(initiative) {
+  proto.onInitiativeMouseoutInSidebar = function (initiative) {
     this.notifyHideInitiativeTooltip(initiative);
   };
-  proto.onMarkerSelectionSet = function(data) {
+  proto.onMarkerSelectionSet = function (data) {
     const initiative = data;
     //console.log(initiative);
     const lastContent = this.contentStack.current();
@@ -214,7 +214,7 @@ define([
     // this.notifySidebarNeedsToShowInitiatives();
     this.view.refresh();
   };
-  proto.onMarkerSelectionToggled = function(data) {
+  proto.onMarkerSelectionToggled = function (data) {
     const initiative = data;
     const lastContent = this.contentStack.current();
     // Make a clone of the current initiatives:
@@ -239,7 +239,7 @@ define([
       topic: "Map.removeSearchFilter",
       data: {}
     });
-    
+
   }
 
   proto.searchedInitiativeClicked = function (id) {
@@ -247,7 +247,7 @@ define([
   }
 
 
-  proto.performSearch = function(text) {
+  proto.performSearch = function (text) {
     console.log("Search submitted: [" + text + "]");
     // We need to make sure that the search sidebar is loaded
     if (text.length > 0) {
@@ -271,13 +271,13 @@ define([
     else {
       //TODO: do other stuff like panning
       //causes err?
-      eventbus.publish({topic: "Map.removeSearchFilter"});
-      eventbus.publish({topic: "Sidebar.showDirectory"});
+      eventbus.publish({ topic: "Map.removeSearchFilter" });
+      eventbus.publish({ topic: "Sidebar.showDirectory" });
     }
   };
 
 
-  proto.changeSearchText = function(txt){
+  proto.changeSearchText = function (txt) {
     this.view.changeSearchText(txt);
   };
 
@@ -290,7 +290,7 @@ define([
     p.registerView(view);
     eventbus.subscribe({
       topic: "Search.initiativeResults",
-      callback: function(data) {
+      callback: function (data) {
         p.onInitiativeResults(data);
       }
     });
@@ -305,40 +305,40 @@ define([
 
     eventbus.subscribe({
       topic: "Marker.SelectionToggled",
-      callback: function(data) {
+      callback: function (data) {
         p.onMarkerSelectionToggled(data);
       }
     });
     eventbus.subscribe({
       topic: "Marker.SelectionSet",
-      callback: function(data) {
+      callback: function (data) {
         p.onMarkerSelectionSet(data);
       }
     });
     eventbus.subscribe({
       topic: "Directory.initiativeClicked",
-      callback: function(data) {
+      callback: function (data) {
         p.onInitiativeClickedInSidebar(data);
       }
     });
 
     eventbus.subscribe({
       topic: "Initiatives.showSearchHistory",
-      callback: function(data){
+      callback: function (data) {
         p.onSearchHistory()
       }
     });
 
     eventbus.subscribe({
       topic: "Initiatives.searchedInitiativeClicked",
-      callback: function(data){
+      callback: function (data) {
         p.searchedInitiativeClicked(data.initiative.uniqueId)
       }
     });
 
     eventbus.subscribe({
       topic: "Search.changeSearchText",
-      callback: function(data){
+      callback: function (data) {
         p.changeSearchText(data.txt)
       }
     });
