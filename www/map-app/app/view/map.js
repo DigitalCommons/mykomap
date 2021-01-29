50710d17b124c79fc1b0c44c4abd0f86c289b819
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-01-29 23:53:19 +0000

    map-app/app/view/map.js - load leaflet.css and image workaround code
    Borrowed from https://github.com/btpschroeder/leaflet-webpack/blob/master/src/index.js

--- www/map-app/app/view/map.js
+++ www/map-app/app/view/map.js
@@ -22,6 +22,18 @@
   
   "use strict";
 
+
+/* This code is needed to properly load the stylesheet, and images in the Leaflet CSS */
+const leafletCss = require('leaflet/dist/leaflet.css');
+delete leaflet.Icon.Default.prototype._getIconUrl;
+leaflet.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
+
 function init(registry) {
   const _config = registry('config');
   const presenter = registry('presenter/map');
