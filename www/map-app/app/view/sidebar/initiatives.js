869a5e2173d2eca633a2bd332ba483dca2e0f888
John Evans <38507954+King-Mob@users.noreply.github.com>
2021-02-23 12:39:20 +0000

    view\sidebar\initiatives.js dropdowns are sorted alphabetically

--- www/map-app/app/view/sidebar/initiatives.js
+++ www/map-app/app/view/sidebar/initiatives.js
@@ -258,6 +258,7 @@ define([
     const currentFilters = map.getFilters();
     const item = presenter.currentItem();
 
+    //function used in the dropdown to change the filter
     const changeFilter = () => {
       //create the filter from the event of selecting the option
       const filterCategoryName = d3.event.target.id.split("-")[0];
@@ -300,7 +301,10 @@ define([
           .attr("value","any")
           .attr("class","advanced-option")
 
-      for(const value in values[field]){
+      const valueArray = Object.keys(values[field]);
+      valueArray.sort(); //sort alphabetically
+
+      valueArray.forEach(value=>{
         const option = dropDown
           .append("option")
           .text(values[field][value])
@@ -317,7 +321,7 @@ define([
               option.attr("disabled",true);
             }
         }
-      }
+      })
     }
   }
 
