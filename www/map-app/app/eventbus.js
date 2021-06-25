67efff0e0f447c9f976f9ff7fee4230391e5c44d
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-06-25 15:36:59 +0100

    www/map-app/app/eventbus.js - add commented event tap
    just for future use

--- www/map-app/app/eventbus.js
+++ www/map-app/app/eventbus.js
@@ -4,8 +4,12 @@
 define(["postal"], function(postal) {
   "use strict";
 
-  // TODO - If we need to configure postal for our use, do it here.
-
+  // TODO - If we need to configure postal for our use, do it here
+  /* This example reports all events
+  var tap = postal.addWireTap( function( d, e ) {
+    console.log("POSTAL:", e);
+  });
+  */
   // For now, we expose all of postal as the interface to our eventbus
   return postal;
 });
