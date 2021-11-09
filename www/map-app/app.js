4f2a057f3f2572ab2df1f881a536928ba53b23c5
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-11-09 18:53:49 +0000

    map-app/app.js - fetch config.json etc. with relative path
    an absolute path won't work on sites like owned-by-oxford-project,
    which are not deployed at the top level.

--- www/map-app/app.js
+++ www/map-app/app.js
@@ -6,12 +6,15 @@
 // (as they were before we used webpack), but also so they can be queried (by shipshape)
 // and updated manually in a deployed site when necessary.
 //
+// The paths are deliberately relative, to allow sea-map applications to be deployed at
+// arbitrary paths.
+//
 // Specify this as an entry point in webpack as "sea-map/www/map-app/app.js"
 
 const main = require("./app/main");
-const config = fetch("/configuration/config.json");
-const versions = fetch("/configuration/version.json");
-const about = fetch("/configuration/about.html");
+const config = fetch("configuration/config.json");
+const versions = fetch("configuration/version.json");
+const about = fetch("configuration/about.html");
 const getJson = (r) => r.json();
 const getText = (r) => r.text();
 Promise
