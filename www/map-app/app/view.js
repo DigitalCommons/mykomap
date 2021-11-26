e1d94f6c27eac9cfc96af7a3e82452a910057cd3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-11-26 00:35:11 +0000

    app/view.js - set the title from titles in about.html
    If present. Use the language-specific one preferentially, else one
    with no language. But any configured title will override all of this.

--- www/map-app/app/view.js
+++ www/map-app/app/view.js
@@ -7,15 +7,48 @@ function _init(registry) {
   const map = registry('view/map');
   const sidebar = registry('view/sidebar');
   const searchbox = registry('view/searchbox');
+  const lang = config.getLanguage();
   
   function init() {
-    const title = config.htmlTitle();
-    if (title) {
-      d3.select("html")
-        .select("head")
-        .select("title")
-        .text(title);
+    // We need to be careful to guard against weird characters, especially quotes,
+    // from the language code, as these can create vulnerabilities.
+    if (lang.match(/[^\w -]/))
+      throw new Error(`rejecting suspect language code '${lang}'`);
+    
+    let title = config.htmlTitle();
+    const about = config.aboutHtml();
+    
+    if (!title && about) {
+      // Look for a title tag in about.html
+
+      // Create a temp node to inspect the about html
+      const temp =
+        d3.select("head")
+          .insert("div") // make a temp node
+          .html(about) // insert the whole text temporarily
+
+      // Try title[lang=$lang] first
+      let tempTitleNode =
+        temp.select(`title[lang='${lang}']`);
+
+      // Fall back to title with no lang attribute
+      if (tempTitleNode.empty())
+        tempTitleNode = temp.select('title:not([lang])');
+
+      if (!tempTitleNode.empty())
+        title =
+          tempTitleNode.text(); // Get the text
+
+      // Remove the temp node
+      temp.remove();
     }
+
+    // Set the title, if available
+    if (title) {
+      const titleNode = d3.select("html head title");
+      titleNode.text(title);
+    }
+    
     // @todo - make obsolete
     d3.select("#about-btn")
       .attr("title", "See information about this app in new tab")
