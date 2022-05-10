20563f58dfbaa94d9eb671cd2e103c776fe02443
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-05-10 14:54:29 +0100

    app/presenter.js - convert into a class definition

--- www/map-app/app/presenter.js
+++ www/map-app/app/presenter.js
@@ -1,14 +1,9 @@
 "use strict";
 
 // 'Base class' for all presenters:
-var base = function() {};
-base.prototype = {
-  view: null,
-  registerView: function(v) {
+export class base {
+  view = null;
+  registerView(v) {
     this.view = v;
   }
-};
-var pub = {
-  base: base
-};
-module.exports = pub;
+}
