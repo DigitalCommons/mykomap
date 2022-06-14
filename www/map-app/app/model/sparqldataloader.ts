5b43ac314e0ccb2b061ee6918f7a873983c71512
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-06-14 21:16:58 +0100

    app/model/sparqldataloader.ts - pre-process multi-record instances into single ones
    So this:
    
        [{ uri: 'xxx', multivalue: 1 },
         { uri: 'xxx', multivalue: 2 },
         { uri: 'yyy', multivalue: 3 },
         { uri: 'zzz', multivalue: 4 }]
    
    Becomes this:
    
        [{ uri: 'xxx', multivalue: [1,2] },
         { uri: 'yyy', multivalue: 3 },
         { uri: 'zzz', multivalue: 4 }]
    
    Note that yyy and zzz's multivalue fields are represented by a single
    non-array value. This is due to the lack of any metadata at the time
    of pre-processing.
    
    The SparqlDataAggregator then turns these objects into Initiatives in
    one step. This makes it simpler to switch to a source which is not
    coming from sparql (like a CSV).

--- www/map-app/app/model/sparqldataloader.ts
+++ www/map-app/app/model/sparqldataloader.ts
@@ -66,8 +66,22 @@ export class SparqlDataLoader implements DataLoader {
           
           // Load initiatives in chunks, to keep the UI responsive
           while(dataset.length > 0) {
-            const batch = dataset.splice(0, this.maxInitiativesToLoadPerFrame);
+            const batch: InitiativeObj[] = [];
 
+            // We have to be aware that an initiative can be spread
+            // across several records, due to the way that a SPARQL
+            // response encodes fields with multiple values as
+            // multiple records with the non-multiple fields
+            // duplicated, and the multiple fields varying.
+            //
+            // Therefore, read in records with the same uri all at once
+            while(batch.length < this.maxInitiativesToLoadPerFrame) {
+              const item = this.readInitiativeObj(dataset);
+              if (item === undefined) break; // stop if we run out of items
+
+              batch.push(item);
+            }
+            
             // Call addInitiatives in the background, to prevent it blocking other processes.
             await (async () => consumer.addBatch(batch))();
           }
@@ -86,6 +100,77 @@ export class SparqlDataLoader implements DataLoader {
 
     // Having loaded all we can, finish off
     consumer.complete();
+  }
+
+  // Amalgamates multiple records into the same InitiativeObj if they
+  // have the same uri.
+  //
+  // Note that this has to work on local context, so it cannot spot
+  // multiple records when there is only one, or when they are null. 
+  // So this:
+  //
+  //     [{ uri: 'xxx', multivalue: 1 },
+  //      { uri: 'xxx', multivalue: 2 },
+  //      { uri: 'yyy', multivalue: undefined },
+  //      { uri: 'zzz', multivalue: 4 }]
+  //
+  // Becomes this:
+  //
+  //     [{ uri: 'xxx', multivalue: [1,2] },
+  //      { uri: 'yyy', multivalue: undefined },
+  //      { uri: 'zzz', multivalue: 4 }]
+  //
+  // Note that yyy and zzz's multivalue fields are represented by a
+  // single non-array value. This is due to the lack of any metadata
+  // at the time of pre-processing. Note also that this applies to
+  // nulls or undefineds, which need to be interpreted downstream as
+  // empty lists, not a list of a single undefined.
+  //
+  // The SparqlDataAggregator then turns these objects into
+  // Initiatives in one step. This makes it simpler to switch to a
+  // source which is not coming from sparql (like a CSV).  It also
+  // means, at a pinch, that this algorithm can still consume the old
+  // data.
+  //
+  // The plan is to make the server send more sensibly formatted
+  // multiple fields later.
+private readInitiativeObj(records: InitiativeObj[]): InitiativeObj | undefined {
+    if (records.length === 0)
+      return undefined; // No more records
+
+    const first = records.shift(); // Read the first
+
+    // Add any more with the same uri Note, we're a bit limited by
+    // what we can infer without having access to the expected
+    // property schema, or any metadata from SPARQL about the field
+    // types.  If we get multiple identical values in a multivalue
+    // field, we might not be able to tell it isn't single valued.
+    // Live with this for now, later move this code to the
+    // server-side, where this metadata is available.
+    while(records.length > 0 && records[0].uri === first.uri) {
+      const next = records.shift();
+      Object.entries(next)
+        .forEach(entry => {
+          const [key, val] = entry;
+
+          if (val === first[key]) 
+            return; // Infer this is not a multi-value property, do nothing.
+
+          // Add this new value to the multi-valued property. Also ensures the above check will
+          // continue for all subsequent values.
+          if (typeof first[key] === 'object' && first[key] instanceof Array) {
+             // Add the value to an existing array
+            first[key].push(val);
+          }
+          else {
+            // This is the first value, create the array, retaining this and the previous value
+            first[key] = [first[key], val];
+          }
+        });
+    }
+
+    // Done
+    return first;
   }
 
   // Calls this.loadDataset and handles the result, including event emission
