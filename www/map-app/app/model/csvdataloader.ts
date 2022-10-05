cc20aa4465892be3676493563cba8eb6ab072561
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-10-05 12:56:34 +0100

    app/model/csvdataloader.ts - implement remapping of fields

--- www/map-app/app/model/csvdataloader.ts
+++ www/map-app/app/model/csvdataloader.ts
@@ -1,14 +1,14 @@
 import type { Dictionary } from '../../common_types';
 
 import {
-  DatasetMeta,
   DataLoader,
   DataConsumer,
-  AggregatedData,
 } from './dataloader';
 
 import {
   InitiativeObj,
+  PropDef,
+  PropDefs,
 } from './dataservices';
 
 import {
@@ -16,33 +16,41 @@ import {
 } from './config';
 
 import {
+  ObjTransform,
+  ObjTransformFunc,
+  mkObjTransformer,
+  Transforms as T,
+} from '../../obj-transformer';
+
+import {
   parse,
   ParseError,
   ParseRemoteConfig,
   ParseResult,
   ParseStepResult,
-  Parser,
+  Parser as PapaParser,
 } from "papaparse";
 
 
-export class CsvDataLoader implements DataLoader {
-  constructor(readonly id: string, readonly url: string) {}
+export class CsvDataLoader<R extends object = string[]> implements DataLoader {
+  constructor(readonly id: string, readonly url: string, readonly rowTransform: ObjTransformFunc<R,InitiativeObj>) {}
   
   load<T extends DataConsumer>(dataConsumer: T): Promise<this> {
     const executor = (resolve: (v: this | PromiseLike<this>) => void , reject: (r?: any) => void) => {
-      const onStep = (results: ParseStepResult<InitiativeObj>, parser: Parser) => {
-        if (results.errors.length == 0) {
+      const onStep = (result: ParseStepResult<R>, parser: PapaParser) => {
+        if (result.errors.length == 0) {
           // console.debug("CsvDataLoader step", results.data);
-          dataConsumer.addBatch(this.id, [results.data])
+          const initiative = this.rowTransform(result.data);
+          dataConsumer.addBatch(this.id, [initiative])
         }
         else {
           // console.debug("CsvDataLoader step error", results.errors);
-          results.errors
+          result.errors
             .filter((e): e is ParseError => !!e)
             .forEach(e => reject(e));
         }
       };
-      const onComplete = (results: ParseResult<InitiativeObj>) => {
+      const onComplete = (results: ParseResult<R>) => {
         // console.debug("CsvDataLoader complete", results);
         resolve(this);
       };
@@ -50,7 +58,7 @@ export class CsvDataLoader implements DataLoader {
         // console.log("error", error);
         reject(error);
       };
-      const config: ParseRemoteConfig<InitiativeObj> = {
+      const config: ParseRemoteConfig<R> = {
 	      download: true,
 	      header: true,
         step:  onStep,
