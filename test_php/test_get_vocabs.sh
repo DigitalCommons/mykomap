c2fe226f2e8e2e8201b7f2361e97341ec504f36b
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-11-08 15:48:02 +0000

    test_php/test_get_vocabs.sh - get this nominally working again

--- test_php/test_get_vocabs.sh
+++ test_php/test_get_vocabs.sh
@@ -6,7 +6,20 @@ export SEA_MAP_CONFIG_DIR=$(readlink -f config)
 
 
 set errexit pipefail
-cd ../www/services
+cd ../www/
 
-php ./get_vocabs.php
+
+export REDIRECT_STATUS=200
+export REQUEST_METHOD=POST
+export SCRIPT_FILENAME=services/get_vocabs.php
+export SCRIPT_NAME=/services/get_vocabs.php
+export PATH_INFO=/
+export SERVER_NAME=example.com
+export SERVER_PROTOCOL=HTTP/1.1
+export REQUEST_URI=/services/get_vocabs.php
+export HTTP_HOST=example.com
+export CONTENT_LENGTH=$(wc -c $SEA_MAP_CONFIG_DIR/config.json)
+
+# This currently doesn't check anything, just prints out the response
+php-cgi ./services/get_vocabs.php  <$SEA_MAP_CONFIG_DIR/config.json 
 
