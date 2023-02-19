307d7288ed7627db8d79bee19e21d4da7aac6495
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-02-19 15:12:21 +0000

    map-app/utils.ts - explicitly allow null/undefined in arrayMax/arrayMin

--- src/map-app/utils.ts
+++ src/map-app/utils.ts
@@ -1,9 +1,9 @@
 
 
-export function arrayMax(array: number[]) {
+export function arrayMax(array: Array<number|undefined|null>): number {
   return array.reduce((a, b) => Math.max(a ?? Number.NEGATIVE_INFINITY, b ?? Number.NEGATIVE_INFINITY));
 }
 
-export function arrayMin(array: number[]) {
+export function arrayMin(array: Array<number|undefined|null>): number {
   return array.reduce((a, b) => Math.min(a ?? Number.POSITIVE_INFINITY, b ?? Number.POSITIVE_INFINITY));
 }
