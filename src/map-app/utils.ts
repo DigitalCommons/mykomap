966936401df09d29d1da10a830a5874967f3b2bd
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-02-19 16:02:50 +0000

    map-app/utils.ts - ensure no null/undefined types returned from arrayMax/arrayMin

--- src/map-app/utils.ts
+++ src/map-app/utils.ts
@@ -1,9 +1,24 @@
 
 
-export function arrayMax(array: Array<number|undefined|null>): number {
-  return array.reduce((a, b) => Math.max(a ?? Number.NEGATIVE_INFINITY, b ?? Number.NEGATIVE_INFINITY));
+type numberFn = (a: number|undefined|null, b: number|undefined|null) => number;
+type numberAryFn = (array: Array<number|undefined|null>) => number;
+
+const max: numberFn = (a, b) =>  Math.max(a ?? Number.NEGATIVE_INFINITY, b ?? Number.NEGATIVE_INFINITY);
+const min: numberFn = (a, b) =>  Math.min(a ?? Number.POSITIVE_INFINITY, b ?? Number.POSITIVE_INFINITY);
+
+/// Constructs a function which, given a numberFn, returns a numberAryFn
+/// that essentally reduces the array but does so without leaking null or undefined
+/// as a function based on Array.reduce would.
+function reducer(fn: numberFn): numberAryFn {
+  return (ary) => {
+    let prev = fn(undefined, undefined);
+    for(const item of ary) {
+      prev = fn(prev, item);
+    }
+    return prev;
+  }
 }
 
-export function arrayMin(array: Array<number|undefined|null>): number {
-  return array.reduce((a, b) => Math.min(a ?? Number.POSITIVE_INFINITY, b ?? Number.POSITIVE_INFINITY));
-}
+export const arrayMax: numberAryFn = reducer(max);
+export const arrayMin: numberAryFn = reducer(min);
+
