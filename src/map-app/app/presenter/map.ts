390a0d23d88fdae7d6ec00bcf34e25ebc5e08aea
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-02-28 18:36:52 +0000

    app/view/map/marker.ts - move ExtendedMarker into app/map.js

--- src/map-app/app/presenter/map.ts
+++ src/map-app/app/presenter/map.ts
@@ -2,12 +2,12 @@ import { BasePresenter } from './base';
 import * as d3 from 'd3';
 import { EventBus } from '../../eventbus';
 import * as leaflet from 'leaflet';
-import { ExtendedMarker } from '../view/map/marker';
 import { Dictionary } from '../../common_types';
 import { MapView } from '../view/map';
 import { Initiative } from '../model/initiative';
 import { toString as _toString } from '../../utils';
 import { MapUI } from '../mapui';
+import { ExtendedMarker } from '../map';
 
 export class MapPresenter extends BasePresenter {
   readonly view: MapView;
