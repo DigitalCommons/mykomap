d33b2eca71cc71d5a93cdb3129bf793e2eedacce
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-03-09 23:00:57 +0000

    app/view/map.ts - segregate event handlers by method access
    Put the presenter stuff in the presenter (in new .on* methods), the
    view stuff in the view.

--- src/map-app/app/presenter/map.ts
+++ src/map-app/app/presenter/map.ts
@@ -1,8 +1,5 @@
 import { BasePresenter } from './base';
-import * as d3 from 'd3';
 import { EventBus } from '../../eventbus';
-import * as leaflet from 'leaflet';
-import { Dictionary } from '../../common-types';
 import { MapView } from '../view/map';
 import { Initiative } from '../model/initiative';
 import { initiativeUris, toString as _toString } from '../../utils';
@@ -53,57 +50,8 @@ export class MapPresenter extends BasePresenter {
     EventBus.Map.removeSearchFilter.sub(() => this.removeSearchFilter());
   }
     
-  private static copyTextToClipboard(text: string) {
-    const body = d3.select(document.body)
-    const textArea = body.append("textarea")
-      .attr(
-        "style",
-        // Place in top-left corner of screen regardless of scroll position.
-        "position: fixed; top: 0; left: 0; "+
-          // Ensure it has a small width and height. Setting to 1px / 1em
-          // doesn't work as this gives a negative w/h on some browsers.
-          "width: 2em; height: 2em; "+
-          // We don't need padding, reducing the size if it does flash render.
-          "padding: 0; "+
-          // Clean up any borders.          
-          "border: none; outline: none; box-shadow: none; "+
-          // Avoid flash of white box if rendered for any reason.
-          "background: transparent"
-           )
-      .attr("value", text)
-    
-    // ***taken from https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript?page=1&tab=votes#tab-top ***
-    //
-    // *** This styling is an extra step which is likely not required. ***
-    //
-    // Why is it here? To ensure:
-    // 1. the element is able to have focus and selection.
-    // 2. if element was to flash render it has minimal visual impact.
-    // 3. less flakyness with selection and copying which **might** occur if
-    //    the textarea element is not visible.
-    //
-    // The likelihood is the element won't even render, not even a
-    // flash, so some of these are just precautions. However in
-    // Internet Explorer the element is visible whilst the popup
-    // box asking the user for permission for the web page to
-    // copy to the clipboard.
-    //
-
-    textArea.node()?.focus();
-    textArea.node()?.select();
-
-    try {
-      document.execCommand('copy'); // FIXME this method has been deprecated!
-    } catch (err) {
-      console.log('Oops, unable to copy', err);
-    }
-
-    textArea.remove()
-  }
-
   createMap(): Map {
     const map = this.view.createMap(
-      this.getMapEventHandlers(),
       this.getMapAttribution(),
       this.getDisableClusteringAtZoomFromConfig(),
       this.mapUI.config.getTileUrl()
@@ -125,37 +73,23 @@ export class MapPresenter extends BasePresenter {
   getMapAttribution() {
     return this.mapUI.config.getMapAttribution();
   }
-  
-  getMapEventHandlers(): Dictionary<leaflet.LeafletEventHandlerFn> {
-    return {
-      click: (e: leaflet.LeafletEvent) => {
-        const me = e as leaflet.LeafletMouseEvent; // Coersion seems to be unavoidable?
-        // Deselect any selected markers        
-        if (me?.originalEvent?.ctrlKey && me?.latlng) {
-          MapPresenter.copyTextToClipboard(me.latlng.lat + "," + me.latlng.lng);
-        }
 
-        EventBus.Directory.initiativeClicked.pub(undefined);
-      },
-      load: (_: leaflet.LeafletEvent) => {
-        console.log("Map loaded");
-
-        let defaultOpenSidebar = this.mapUI.config.getDefaultOpenSidebar();
-
-        // Trigger loading of the sidebar, the deps should all be in place now.
-        this.mapUI.getSidebarPresenter(this.mapUI).then(sidebar => {
-          if (defaultOpenSidebar)
-            sidebar.showSidebar()
-        });
-
-      },
-      resize: (_: leaflet.LeafletEvent) => {
-        this.view.map?.invalidateSize();
-        console.log("Map resize", window.outerWidth);
-      }
-    };
+  onInitiativeClicked() {
+    EventBus.Directory.initiativeClicked.pub(undefined);
   }
 
+  onLoad() {
+    console.log("Map loaded");
+    
+    let defaultOpenSidebar = this.mapUI.config.getDefaultOpenSidebar();
+    
+    // Trigger loading of the sidebar, the deps should all be in place now.
+    this.mapUI.getSidebarPresenter(this.mapUI).then(sidebar => {
+      if (defaultOpenSidebar)
+        sidebar.showSidebar()
+    });
+  }
+  
   private onInitiativeNew(initiative: Initiative) {
     this.view.addMarker(initiative);
   }
--- src/map-app/app/view/map.ts
+++ src/map-app/app/view/map.ts
@@ -1,4 +1,3 @@
-import { Box2d, Dictionary } from "../../common-types";
 import { MapPresenter } from "../presenter/map";
 import { BaseView } from './base';
 import { Map } from '../map';
@@ -9,6 +8,7 @@ import { Initiative } from "../model/initiative";
 import { EventBus } from "../../eventbus";
 import { MarkerManager } from "../marker-manager";
 import { PhraseBook } from "../../localisations";
+import { Box2d } from "../../common-types";
 
 export class MapView extends BaseView {
   map?: Map;
@@ -37,6 +37,55 @@ export class MapView extends BaseView {
       e.layer.off('data:loading');
     }, this);
   }
+
+  private static copyTextToClipboard(text: string) {
+    const body = d3.select(document.body)
+    const textArea = body.append("textarea")
+      .attr(
+        "style",
+        // Place in top-left corner of screen regardless of scroll position.
+        "position: fixed; top: 0; left: 0; "+
+          // Ensure it has a small width and height. Setting to 1px / 1em
+          // doesn't work as this gives a negative w/h on some browsers.
+          "width: 2em; height: 2em; "+
+          // We don't need padding, reducing the size if it does flash render.
+          "padding: 0; "+
+          // Clean up any borders.          
+          "border: none; outline: none; box-shadow: none; "+
+          // Avoid flash of white box if rendered for any reason.
+          "background: transparent"
+           )
+      .attr("value", text)
+    
+    // ***taken from https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript?page=1&tab=votes#tab-top ***
+    //
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a
+    // flash, so some of these are just precautions. However in
+    // Internet Explorer the element is visible whilst the popup
+    // box asking the user for permission for the web page to
+    // copy to the clipboard.
+    //
+
+    textArea.node()?.focus();
+    textArea.node()?.select();
+
+    try {
+      document.execCommand('copy'); // FIXME this method has been deprecated!
+    } catch (err) {
+      console.log('Oops, unable to copy', err);
+    }
+
+    textArea.remove()
+  }
+
   
   /// Initialises the view, but does not create the map yet.
   ///
@@ -74,7 +123,26 @@ export class MapView extends BaseView {
 
   }
 
-  createMap(eventHandlers: Dictionary<leaflet.LeafletEventHandlerFn>, mapAttribution: string, disableClusteringAtZoom: number|false, tileUrl?: string): Map {
+  private onInitiativeClicked(me: leaflet.LeafletMouseEvent): void {
+    // Deselect any selected markers        
+    if (me.originalEvent.ctrlKey && me.latlng) {
+      MapView.copyTextToClipboard(me.latlng.lat + "," + me.latlng.lng);
+    }
+    this.presenter.onInitiativeClicked();
+  }
+  
+  private onLoad(_: leaflet.LeafletEvent) {
+    this.presenter.onLoad();
+  }
+  
+  private onResize(_: leaflet.ResizeEvent) {
+    this.map?.invalidateSize();
+    console.log("Map resize", window.outerWidth);
+  }
+
+
+
+  createMap(mapAttribution: string, disableClusteringAtZoom: number|false, tileUrl?: string): Map {
     document.body.appendChild(this.dialogueSizeStyles);
     
     // setup map (could potentially add this to the map initialization instead)
@@ -108,13 +176,10 @@ export class MapView extends BaseView {
     
     this.map.zoomControl.setPosition("bottomright");
 
-    for (const key in eventHandlers) {
-      const eventHandler = eventHandlers[key];
-      if (eventHandler)
-        this.map.on(key, eventHandler);
-    }
-
-
+    this.map.on('click', (e) => this.onInitiativeClicked(e));
+    this.map.on('load', (e) => this.onLoad(e));
+    this.map.on('resize', (e) => this.onResize(e));
+    
     leaflet
       .tileLayer(tileMapURL, { attribution: osmAttrib, maxZoom: 17, noWrap: true })
       .addTo(this.map);
