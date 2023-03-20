588d2b01c078380dca1bba74603c3efa0f83f35a
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-03-20 13:13:01 +0000

    .dir-locals.el - emacs project configs

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Emacs Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((project-compilation-mode . clojure-compilation-mode))))
