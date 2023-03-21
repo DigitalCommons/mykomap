c4a786096b236f1cf4fdfbe982dd0ade74208256
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-03-21 20:59:16 +0000

    app/map-ui.ts - implement getSearchDescription() somewhat
    So that something appears - even if it isn't fully localised yet.

--- src/map-app/app/map-ui.ts
+++ src/map-app/app/map-ui.ts
@@ -308,13 +308,26 @@ export class MapUI {
   //
   // Includes text and filtering.
   getSearchDescription(): string {
-    //throw new Error();
-    /*
-    FIXME
-    const lastSearchResults = this.stateManager.currentState;
-    if (lastSearchResults instanceof SearchResults)
-    	return `${this.labels.search}: ${lastSearchResults.searchString}`;*/
-    return "FIXME";
+    const state = this.currentItem();
+    const descr: string[] = [];
+    if (state.hasTextSearch) {
+      // FIXME use a better label, like "Searching for text"
+      descr.push(`${this.labels.search} "${state.textSearch.searchText}"`);
+    }
+
+    if (state.hasPropFilters) {
+      // FIXME use localised field names
+      const names = Object.keys(state.propFilters).sort();
+      const filterNames = names.map(name => {
+        const filter = state.propFilters[name];
+        if (filter)
+          return `${name}: ${filter.valueRequired}`;
+        return undefined;
+      });
+      descr.push(...compactArray(filterNames));
+    }
+
+    return descr.join(` ${this.labels.and} `);
   }
 
   
