a396e097701c49d18004625cfcfda0247aeadf93
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-03-30 10:26:34 +0100

    .dir-locals.el - emacs project configs
    Useful for users of emacs, i.e. Nick. Shouldn't affect anyone else.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Emacs Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((project-compilation-mode . clojure-compilation-mode))))
+((nil . ((project-compilation-mode . js-compilation-mode)
+	 (compile-command . "npm ci && npm run build && npm run server "))))
