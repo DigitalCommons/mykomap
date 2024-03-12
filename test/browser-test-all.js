4fd3a33f4696d45d962fb22b17040a21f17733c8
rogup <rohit5.gup@gmail.com>
2024-03-12 17:10:38 +0000

    Fix GitHub CI pipeline and add all browser tests

--- test/browser-test-all.js
+++ test/browser-test-all.js
@@ -1,2 +1,12 @@
 // tests for running via karma in the browser
-require('./test-model-config');
+
+// FIXME TEST NOT WORKING: import './test-csvdataloader';
+import './test-dataaggregator';
+import './test-directory-construction';
+import './test-model-config';
+import './test-obj-transformer';
+// FIXME TEST NOT WORKING: import './test-papaparse';
+// FIXME TEST NOT WORKING: import './test-sparqldataloader';
+import './test-view-map-popup';
+import './test-vocab-services';
+
